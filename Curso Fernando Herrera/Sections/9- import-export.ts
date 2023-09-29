b496d8a6cd198742a9a2b6bc4281acca91f5cb43
Hector Ivan <hirfleites@gmail.com>
2023-09-28 21:25:23 -0400

    commit 2

---
+++ Curso Fernando Herrera/Sections/9- import-export.ts
@@ -0,0 +1,18 @@
+import { addTwoNumbers, divideTwoNumbers, substractTwoNumbers, timesTwoNumbers } from "../helpers/math-helpers"
+
+ 
+
+
+let total1 = addTwoNumbers(10,20)
+let total2 = substractTwoNumbers(10,20)
+let total3 = timesTwoNumbers(10,20)
+let total4 = divideTwoNumbers(10,20)
+
+console.log("Suma: ", total1)
+console.log("Resta: ", total2)
+console.log("Multiplicación: ", total3)
+console.log("División: ", total4)
+
+
+// let firstName = "Héctor"
+// greet(firstName)
